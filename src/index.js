e43a4b8c53fa47d1bac6c3ca76e6f7c03cb1f40a
Martin Prévost <martin.mprevost@gmail.com>
2024-03-20 10:09:53 +0100

    Add URL_BACK variable and update endpoint

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import * as login from "./services/login.js";
 import * as user from "./services/user.js";
 
 const swaggerFile = JSON.parse(fs.readFileSync("./src/swagger/swagger-output.json", "utf-8"));
+const URL_BACK = `${process.env.URL_BACK}:${process.env.PORT}`;
 
 dotenv.config();
 
@@ -23,6 +24,8 @@ const corsOptions = {
 const uploadUser = multer({ dest: 'public/users/' });
 
 app.use(cors(corsOptions), bodyParser.json());
+app.get("/", (req, res) => { res.send(`API de SkillMINER, documentation ${URL_BACK}/swagger`); });
+
 app.post("/login", login.login);
 app.post("/reset-request", login.resetRequest);
 app.post("/reset-password", login.resetPassword);
@@ -38,13 +41,11 @@ app.post("/file/users", auth, uploadUser.single('file'), user.uploadPhoto);
 app.use('/file', auth, express.static('public'));
 
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use("/", (req, res) => {
-  res.send("API de SkillMINER, documentation /swagger");
-});
+app.use((req, res) => { res.status(404).send({ error: "Page non trouvée" }); });
 
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(
-    `Serveur en cours d'exécution sur le port ${port}, documentation : /swagger`,
+    `Serveur en cours d'exécution sur le port ${port}, documentation ${URL_BACK}/swagger`,
   );
 });
