49a0f94dece6286c74cd66ce0d0a164cd5d82f73
AlexisP1909 <alexis.pouillieute@gmail.com>
2024-03-28 11:03:06 +0100

    Modif package.json start-watch, index

--- src/index.js
+++ src/index.js
@@ -46,6 +46,7 @@ app.get("/formations/:id", formation.findById);
 app.get("/formations", formation.findAll);
 app.post("/formations", auth, formation.add);
 app.patch("/formations", auth, formation.update);
+app.delete("/formations", auth, formation.deleteWithToken);
 
 app.post("/file/users", auth, uploadUser.single("file"), user.uploadPhoto);
 app.use("/file", auth, express.static("public"));
