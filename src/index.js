0cca44537e67960da275bdbe0fb99138e4966d2c
Martin Prévost <martin.mprevost@gmail.com>
2024-04-04 13:01:50 +0200

    Add gravatar for default and update endpoint for user photo

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ app.post("/tags", auth, tag.add);
 
 app.post("/file/users", auth, uploadUser.single("file"), user.uploadPhoto);
 app.post("/file/formations/:id", auth, uploadFormation.single("file"), formation.uploadPhoto);
-app.use("/file/users", express.static("public/users"), user.sendDefaultPhoto);
+app.get("/file/users/:id", user.sendPhoto);
 app.use("/file/formations", express.static("public/formations"), formation.sendDefaultPhoto);
 
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
